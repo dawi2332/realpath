83e10f16970a1fa04eb56858db0e51e17e0c55c4
dawi2332 <dawi2332@gmail.com>
2017-02-07 01:57:08 +0100

    Replaced README.md, fixed issues with builddir

--- man/local.mk
+++ man/local.mk
@@ -1,9 +1,13 @@
 man1_MANS = man/realpath.1
 CLEANFILES += $(man1_MANS)
-EXTRA_DIST += $(man1_MANS)
+EXTRA_DIST += $(man1_MANS:.1=.1.ronn)
 
 distclean-local:
 	test x$(srcdir) = x$(builddir) || rm -f $(man1_MANS)
 
-man/realpath.1: $(top_srcdir)/README.md
-	$(AM_V_GEN)$(RONN) -r --pipe $< > $@
+SUFFIXES = .1 .1.ronn
+
+.1.ronn.1:
+	$(AM_V_GEN)$(MKDIR_P) $(@D)
+	echo ".ad l" > $@-t
+	$(RONN) -r < $< >> $@-t && mv $@-t $@
