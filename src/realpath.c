ccaba19b6b910ed329779f8b6e93c0712a6d1692
dawi2332 <dawi2332@gmail.com>
2024-02-18 02:19:18 +0100

    Changed code style, updated Copyright and fixed PATH_MAX usage

--- src/realpath.c
+++ src/realpath.c
@@ -1,5 +1,5 @@
 /* realpath.c -- prints resolved absolute pathname.
-   Copyright © 2017 David Winter
+   Copyright © 2017-2024 David Winter
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -15,37 +15,32 @@
    along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
 #include <config.h>
+#include <err.h>
+#include <getopt.h>
+#include <limits.h> // IWYU pragma: keep
+#include <stdbool.h>
 #include <stdio.h>
-#include <limits.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
-#include <getopt.h>
-#include <err.h>
 #include <sysexits.h>
+#include <unistd.h>
 
-enum {
-	RELATIVE_TO_OPTION,
-	RELATIVE_BASE_OPTION,
-	HELP_OPTION,
-	VERSION_OPTION
-};
+enum { RELATIVE_TO_OPTION, RELATIVE_BASE_OPTION, HELP_OPTION, VERSION_OPTION };
 
 static const struct option longopts[] = {
-	{"canonicalize-existing", no_argument, NULL, 'e'},
-	{"canonicalize-missing", no_argument, NULL, 'm'},
-	{"help", no_argument, NULL, HELP_OPTION},
-	{"logical", no_argument, NULL, 'L'},
-	{"no-symlinks", no_argument, NULL, 's'},
-	{"physical", no_argument, NULL, 'P'},
-	{"quiet", no_argument, NULL, 'q'},
-	{"relative-base", required_argument, NULL, RELATIVE_BASE_OPTION},
-	{"relative-to", required_argument, NULL, RELATIVE_TO_OPTION},
-	{"strip", no_argument, NULL, 's'},
-	{"version", no_argument, NULL, VERSION_OPTION},
-	{"zero", no_argument, NULL, 'z'},
-	{NULL, 0, NULL, 0}
-};
+    {"canonicalize-existing", no_argument, NULL, 'e'},
+    {"canonicalize-missing", no_argument, NULL, 'm'},
+    {"help", no_argument, NULL, HELP_OPTION},
+    {"logical", no_argument, NULL, 'L'},
+    {"no-symlinks", no_argument, NULL, 's'},
+    {"physical", no_argument, NULL, 'P'},
+    {"quiet", no_argument, NULL, 'q'},
+    {"relative-base", required_argument, NULL, RELATIVE_BASE_OPTION},
+    {"relative-to", required_argument, NULL, RELATIVE_TO_OPTION},
+    {"strip", no_argument, NULL, 's'},
+    {"version", no_argument, NULL, VERSION_OPTION},
+    {"zero", no_argument, NULL, 'z'},
+    {NULL, 0, NULL, 0}};
 
 static int longindex;
 static const char *program_name = NULL;
@@ -53,106 +48,111 @@ static bool verbose = true;
 static char eol = '\n';
 
 void usage() {
-	printf("Usage: %s [OPTION]... FILE...\n"
-			"Prints the canonicalized absolute pathname of FILE.\n\n"
-			"	-q,	--quiet		suppress error messages\n"
-			"	-z,	--zero		terminate lines with NUL instead of LF\n"
-			"		--help		display this help and exit\n"
-			"		--version	display version information and exit\n", program_name);
-	exit(EXIT_SUCCESS);
+  printf(
+      "Usage: %s [OPTION]... FILE...\n"
+      "Prints the canonicalized absolute pathname of FILE.\n\n"
+      "	-q,	--quiet		suppress error messages\n"
+      "	-z,	--zero		terminate lines with NUL instead of LF\n"
+      "		--help		display this help and exit\n"
+      "		--version	display version information and exit\n",
+      program_name);
+  exit(EXIT_SUCCESS);
 }
 
 void usage_err(const char *msg) {
-	if (msg != NULL) {
-		warnx("%s", msg);
-	}
-	fprintf(stderr, "Try \"%s --help\" for more information.\n", program_name);
-	exit(EXIT_FAILURE);
+  if (msg != NULL) {
+    warnx("%s", msg);
+  }
+  fprintf(stderr, "Try \"%s --help\" for more information.\n", program_name);
+  exit(EXIT_FAILURE);
 }
 
 void version() {
-	printf("%s %s\n"
-			"Copyright © 2017 David Winter\n"
-			"License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>\n"
-			"This is free software: you are free to change and redistribute it.\n"
-			"There is NO WARRANTY, to the extent permitted by law.\n", PACKAGE, VERSION);
-	exit(EXIT_SUCCESS);
+  printf("%s %s\n"
+         "Copyright © 2017-2024 David Winter\n"
+         "License GPLv3+: GNU GPL version 3 or later "
+         "<http://gnu.org/licenses/gpl.html>\n"
+         "This is free software: you are free to change and redistribute it.\n"
+         "There is NO WARRANTY, to the extent permitted by law.\n",
+         PACKAGE, VERSION);
+  exit(EXIT_SUCCESS);
 }
 
 void set_program_name(const char *argv0) {
-	const char *s = strrchr(argv0, '/');
-	program_name = (s != NULL ? s + 1 : argv0);
+  const char *s = strrchr(argv0, '/');
+  program_name = (s != NULL ? s + 1 : argv0);
 }
 
 bool resolve_path(const char *filename) {
-	bool ok = false;
-	char *resolved_path = malloc(sizeof(char) * (PATH_MAX+1));
+  static bool ok = false;
+  char *resolved_path = malloc(sizeof(char) * PATH_MAX);
 
-	if (resolved_path == NULL) {
-		err(EX_OSERR, NULL);
-	}
+  if (resolved_path == NULL) {
+    err(EX_OSERR, NULL);
+  }
 
-	if (realpath(filename, resolved_path) != NULL) {
-		printf("%s%c", resolved_path, eol);
-		ok = true;
-	} else if (verbose) {
-		warn("%s", resolved_path);
-	}
+  if (realpath(filename, resolved_path) != NULL) {
+    printf("%s%c", resolved_path, eol);
+    ok = true;
+  } else if (verbose) {
+    warn("%s", resolved_path);
+  }
 
-	free(resolved_path);
-	return ok;
+  free(resolved_path);
+  return ok;
 }
 
 void warn_ignored(char c) {
-	const struct option *opt = &longopts[longindex];
-	if (opt->val == c) {
-		warnx("the option \"--%s\" is not supported but ignored.", opt->name);
-	} else {
-		warnx("the option \"-%c\" is not supported but ignored.", c);
-	}
+  const struct option *opt = &longopts[longindex];
+  if (opt->val == c) {
+    warnx("the option \"--%s\" is not supported.", opt->name);
+  } else {
+    warnx("the option \"-%c\" is not supported.", c);
+  }
 }
 
 int main(int argc, char **argv) {
-	bool ok = true;
-	int ch;
+  bool ok = true;
+  int ch;
 
-	set_program_name(argv[0]);
+  set_program_name(argv[0]);
 
-	while ((ch = getopt_long(argc, argv, "emhLPqsz", longopts, &longindex)) != -1) {
-		switch (ch) {
-			case 'm':
-			case 'L':
-			case 's':
-			case RELATIVE_TO_OPTION:
-			case RELATIVE_BASE_OPTION:
-			case 'e':
-			case 'P':
-				warn_ignored(ch);
-				break;
-			case HELP_OPTION:
-				usage();
-				break;
-			case 'q':
-				verbose = false;
-				break;
-			case VERSION_OPTION:
-				version();
-				break;
-			case 'z':
-				eol = '\0';
-				break;
-			default:
-				usage_err(NULL);
-		}
-	}
+  while ((ch = getopt_long(argc, argv, "emhLPqsz", longopts, &longindex)) !=
+         -1) {
+    switch (ch) {
+    case 'm':
+    case 'L':
+    case 's':
+    case RELATIVE_TO_OPTION:
+    case RELATIVE_BASE_OPTION:
+    case 'e':
+    case 'P':
+      warn_ignored(ch);
+      break;
+    case HELP_OPTION:
+      usage();
+      break;
+    case 'q':
+      verbose = false;
+      break;
+    case VERSION_OPTION:
+      version();
+      break;
+    case 'z':
+      eol = '\0';
+      break;
+    default:
+      usage_err(NULL);
+    }
+  }
 
-	if (optind >= argc) {
-		usage_err("not enough arguments");
-	}
+  if (optind >= argc) {
+    usage_err("not enough arguments");
+  }
 
-	for (; optind < argc; ++optind) {
-		ok &= resolve_path(argv[optind]);
-	}
+  for (; optind < argc; ++optind) {
+    ok &= resolve_path(argv[optind]);
+  }
 
-	return ok ? EXIT_SUCCESS : EXIT_FAILURE;
+  return ok ? EXIT_SUCCESS : EXIT_FAILURE;
 }
